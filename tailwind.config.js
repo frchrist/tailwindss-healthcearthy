7bd99c807a97a479d7583fab175412a104a5f658
christi@an <coding12online@gmail.com>
2022-04-20 13:26:01 +0000

    HealhtyCarely template setup

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary : "#4D77FF",
+        lightPrimary: "#EDF1FF",
+        black : "#121212",
+        gray : "#696969",
+        lightGray: "#C7C9D9"
+      }
+    },
+  },
+  plugins: [],
+}
